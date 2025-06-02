f88eabd3f00757ecfc02b09c3694b44720482180
HaythemAber <elaberhaythem@gmail.com>
2025-06-02 15:05:18 -0400

    Update HelloController.java

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello world from K8S!";
+        return "Hello from K8S!";
     }
 }
 
